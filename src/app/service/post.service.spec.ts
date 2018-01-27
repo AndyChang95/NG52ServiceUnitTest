87458cf782c0cefb2b13890c2144567371aeed4e
Oomusou Xiao <oomusou@gmail.com>
2018-01-27 16:26:17 +0800

    Service unit test PASS

--- src/app/service/post.service.spec.ts
+++ src/app/service/post.service.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { PostService } from './post.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PostInterfaceToken } from '../interface/injection.token';
+import { Post } from '../../model/post.model';
+import { environment } from '../../environments/environment';
+import { PostInterface } from '../interface/post.interface';
 
 describe('PostService', () => {
-  let postService: PostService;
+  let postService: PostInterface;
+  let mockHttpClient: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,9 +21,64 @@ describe('PostService', () => {
     });
 
     postService = TestBed.get(PostInterfaceToken, PostService);
+    mockHttpClient = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(PostService).toBeTruthy();
+  });
+
+  it(`should list all posts`, () => {
+    /** act */
+    const expected: Post[] = [
+      {
+        id: 1,
+        title: 'Design Pattern',
+        author: 'Dr. Eric Gamma'
+      }
+    ];
+
+    postService.listPosts$().subscribe(posts => {
+      /** assert */
+      expect(posts).toEqual(expected);
+    });
+
+    /** arrange */
+    const mockResponse: Post[] = [
+      {
+        id: 1,
+        title: 'Design Pattern',
+        author: 'Eric Gamma'
+      }
+    ];
+
+    mockHttpClient.expectOne({
+      url: `${environment.apiServer}/posts`,
+      method: 'GET'
+    }).flush(mockResponse);
+  });
+
+  it(`should add post`, () => {
+    /** act */
+    const expected: Post = {
+      id: 1,
+      title: 'OOP',
+      author: 'Sam'
+    };
+
+    postService.addPost(expected).subscribe(post => {
+      /** assert */
+      expect(post).toBe(expected);
+    });
+
+    /** arrange */
+    mockHttpClient.expectOne({
+      url: `${environment.apiServer}/posts`,
+      method: 'POST'
+    }).flush(expected);
+  });
+
+  afterEach(() => {
+    mockHttpClient.verify();
   });
 });
