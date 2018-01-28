62356c41e16152f531747f6fa345fb331654bf74
Oomusou Xiao <oomusou@gmail.com>
2018-01-28 20:28:20 +0800

    Refactoring AppComponent unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PostService } from './service/post.service';
 import { PostInterfaceToken } from './interface/injection.token';
+import { DebugElement } from '@angular/core';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let appComponent: AppComponent;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -19,21 +25,23 @@ describe('AppComponent', () => {
         {provide: PostInterfaceToken, useClass: PostService}
       ]
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture = TestBed.createComponent(AppComponent);
+    appComponent = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    htmlElement = debugElement.nativeElement;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  }));
+
+  it('should create the app', async(() => {
+    expect(appComponent).toBeTruthy();
+  }));
+
+  it(`should have as title 'app'`, async(() => {
+    expect(appComponent.title).toEqual('app');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    expect(htmlElement.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
