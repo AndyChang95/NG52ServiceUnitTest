b0c388bc83eea977c342022fb20f0b48c2a21228
Oomusou Xiao <oomusou@gmail.com>
2018-01-28 20:41:32 +0800

    Component unit test PASS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,16 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PostService } from './service/post.service';
 import { PostInterfaceToken } from './interface/injection.token';
 import { DebugElement } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { PostInterface } from './interface/post.interface';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let appComponent: AppComponent;
   let debugElement: DebugElement;
   let htmlElement: HTMLElement;
+  let postService: PostInterface;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,6 +35,8 @@ describe('AppComponent', () => {
     debugElement = fixture.debugElement;
     htmlElement = debugElement.nativeElement;
     fixture.detectChanges();
+
+    postService = TestBed.get(PostInterfaceToken, PostService);
   }));
 
   it('should create the app', async(() => {
@@ -44,4 +50,38 @@ describe('AppComponent', () => {
   it('should render title in a h1 tag', async(() => {
     expect(htmlElement.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
+
+  it(`should list posts`, () => {
+    const expected$ = Observable.of([
+      {
+        id: 1,
+        title: 'Design Pattern',
+        author: 'Dr. Eric Gamma'
+      }
+    ]);
+
+    /** arrange */
+    spyOn(postService, 'listPosts$').and.returnValue(expected$);
+    /** act */
+    appComponent.onListPostsClick();
+    /** assert */
+    expect(appComponent.posts$).toEqual(expected$);
+  });
+
+  it(`should add post`, () => {
+    const expected$ = Observable.of(
+      {
+        id: 1,
+        title: 'Design Pattern',
+        author: 'Dr. Eric Gamma'
+      }
+    );
+
+    /** arrange */
+    const spy = spyOn(postService, 'addPost').and.returnValue(expected$);
+    /** act */
+    appComponent.onAddPostClick();
+    /** assert */
+    expect(spy).toHaveBeenCalled();
+  });
 });
